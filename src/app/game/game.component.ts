988101d32a740443dcd4400c7f63e757242f82c1
yannick.vaterlaus@students.ffhs.ch <yannick.vaterlaus@windowslive.com>
2024-01-03 14:36:18 +0100

    consol.log

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Game } from '../../models/game';
+
+
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
+  pickCardAnimation = false;
+  currentCard: string = '';
+  game!: Game;
+ 
+  constructor(){}
+
+  ngOnInit(): void {
+    this.newGame()
+  }
+
+  newGame(){
+    this.game = new Game();
+    console.log(this.game);
+  }
+
+  takeCard(){
+    console.log(this.currentCard);
+    this.pickCardAnimation = true;
+    
+  }
 
 }
